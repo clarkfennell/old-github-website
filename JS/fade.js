c3bb220c0de43a2007f7c173b1276288d3147288
Clark Fennell <clarkfennell@mac.com>
2018-07-31 14:33:35 +0100

    commit 8

--- JS/fade.js
+++ JS/fade.js
@@ -39,12 +39,3 @@ function closeNav() {
 var divs = $('div[id^="content-"]').hide(),
 
 				i = 0;
-
-(function cycle()  {
-
-					divs.eq(i).fadeIn(400)
-									 	.delay(5000)
-									 	.fadeOut(400, cycle);
-
-					i = ++i % divs.length;
-})();
