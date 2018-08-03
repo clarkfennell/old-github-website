708519ec09cdb6cd7d223325f3a39a84b21374c9
Clark Fennell <clarkfennell@mac.com>
2018-08-03 15:53:16 +0100

    commit 03.08.18 5

--- JS/mywebjscript.js
+++ JS/mywebjscript.js
@@ -1,10 +1,3 @@
-var aboutmediv = $("aboutme");
-var htmlcssdiv = $("myhtmlcss");
-var jscriptdiv = $("myjscript");
-var mycvdiv = $("mycv");
-var contactmediv = $("contactme");
-var currentdiv = $("currentdiv");
-
 $(function () {
   count = 0;
   wordsArray = ["UX/UI DESIGNER", "FRONT-END DEVELOPER", "WEB DESIGNER", "SOFTWARE DEVELOPER"];
@@ -14,34 +7,46 @@ $(function () {
       $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400).addClass("small-heading");
     });
   }, 2000);
+})
+
+var currentdiv = '#aboutmebtn';
+
+$(document).ready(function() {
+  $('#aboutmebtn').click(function(){
+    cureentdiv.fadOut("slow");
+    $('#aboutme').fadeIn("slow");
+    currentdiv = '#aboutme';
+  });
 });
 
-$("aboutmebtn").click(function() {
-  currentdiv.fadeout();
-  aboutmediv.fadeIn("slow");
-  currentdiv = aboutmediv;
+$(document).ready(function() {
+  $('#myhtmlcssbtn').click(function(){
+    cureentdiv.fadOut("slow");
+    $('#myhtmlcss').fadeIn("slow");
+    currentdiv = '#myhtmlcss';
+  });
 });
 
-$("myhtmlcssbtn").click(function() {
-  currentdiv.fadeout();
-  htmlcssdiv.fadeIn("slow");
-  currentdiv = htmlcssdiv;
+$(document).ready(function() {
+  $('#jscriptbtn').click(function(){
+    cureentdiv.fadOut("slow");
+    $('#myjscript').fadeIn("slow");
+    currentdiv = '#myjscript';
+  });
 });
 
-$("myjscriptbtn").click(function() {
-  currentdiv.fadeout();
-  jscriptdiv.fadeIn("slow");
-  currentdiv = jscriptdiv;
+$(document).ready(function() {
+  $('#mycvbtn').click(function(){
+    cureentdiv.fadOut("slow");
+    $('#mycv').fadeIn("slow");
+    currentdiv = '#mycv';
+  });
 });
 
-$("mycvbtn").click(function() {
-  currentdiv.fadeout();
-  mycvdiv.fadeIn("slow");
-  currentdiv = mycvdiv;
-});
-
-$("contactmebtn").click(function() {
-  currentdiv.fadeout();
-  contactmediv.fadeIn("slow");
-  currentdiv = contactmediv;
+$(document).ready(function() {
+  $('#contactmebtn').click(function(){
+    cureentdiv.fadOut("slow");
+    $('#contactme').fadeIn("slow");
+    currentdiv = '#contactme';
+  });
 });
