2b1e88abeccab1a6829032cd43075059d0834b72
Clark Fennell <clarkfennell@mac.com>
2018-08-04 11:09:19 +0100

    commit 04.08.18 9

--- JS/mywebjscript.js
+++ JS/mywebjscript.js
@@ -42,3 +42,12 @@ $(document).ready( function() {
       } );
   } );
 } );
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
